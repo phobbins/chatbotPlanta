0c76c12d59830fd2cc6d4965068286e607b94eeb
nicolassere <nicosgtr@gmail.com>
2025-06-25 14:58:36 -0300

    fix rag

--- rag/loader.py
+++ rag/loader.py
@@ -1,18 +1,29 @@
-from langchain.vectorstores import Chroma
-from langchain.embeddings import SentenceTransformerEmbeddings
+
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import SentenceTransformerEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import TextLoader
+from langchain_community.document_loaders import DirectoryLoader, TextLoader, PyPDFLoader
+from functools import partial
+from langchain_huggingface import HuggingFaceEmbeddings
 
 
-from langchain.document_loaders import PyPDFLoader, TextLoader
-from langchain.document_loaders import DirectoryLoader
 
-# Cargar todos los .txt y .pdf de una carpeta
+
 def cargar_todos_los_documentos():
     directorio = "rag/documentos"
 
-    loader_txt = DirectoryLoader(directorio, glob="*.txt", loader_cls=TextLoader)
-    loader_pdf = DirectoryLoader(directorio, glob="*.pdf", loader_cls=PyPDFLoader)
+    # Forzar UTF-8 al cargar archivos .txt
+    loader_txt = DirectoryLoader(
+        directorio,
+        glob="*.txt",
+        loader_cls=partial(TextLoader, encoding="utf-8")
+    )
+
+    loader_pdf = DirectoryLoader(
+        directorio,
+        glob="*.pdf",
+        loader_cls=PyPDFLoader
+    )
 
     docs_txt = loader_txt.load()
     docs_pdf = loader_pdf.load()
@@ -20,12 +31,14 @@ def cargar_todos_los_documentos():
     return docs_txt + docs_pdf
 
 
+
 # Cargar y partir texto
 def crear_vectorstore(path="documentos_hortensia.txt"):
+    docs=cargar_todos_los_documentos()
     splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=50)
     chunks = splitter.split_documents(docs)
 
-    embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     db = Chroma.from_documents(chunks, embedding=embeddings, persist_directory="rag/chroma_db")
 
     return db
